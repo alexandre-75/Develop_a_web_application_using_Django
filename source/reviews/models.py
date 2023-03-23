1fd1ec3aed01da0300079b944849909ad55766b4
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-23 11:22:32 +0100

    models

---
+++ source/reviews/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(max_length=1024, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True, upload_to='images')
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class UserFollows(models.Model):
+
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following",)
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followed_by",)
+
+    class Meta:
+        unique_together = (
+            "user",
+            "followed_user",
+        )
+
+    def __str__(self):
+        return f'{self.user.username} follows ' \
+               f'{self.followed_user.username}'
