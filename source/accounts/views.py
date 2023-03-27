24d7b7920de31c8aad17e38873c7536108a87743
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-27 10:32:05 +0200

    modification login view.py

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from accounts.models import CustomUser
 from django.http import HttpResponse
+from django.contrib.auth import login
 
 
 def homepage_welcome(request):
@@ -13,7 +14,9 @@ def sign_up(request):
         password2 = request.POST.get("password2")
         if password1 != password2:
             return render(request, "accounts/sign_up.html", {"error": "mdp no correct"})
-        CustomUser.objects.create_user(username=username, password=password1)
-        return HttpResponse(f"bienvenue {username}")
+        user = CustomUser.objects.create_user(username=username, password=password1)
+        login(request, user)
+        return redirect("reviews-main")
+        # return HttpResponse(f"bienvenue {username}")
 
     return render(request, "accounts/sign_up.html")
