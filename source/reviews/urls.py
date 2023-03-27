51237825f31788f68c6a42b08e0dde7ca23152ba
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-27 15:01:34 +0200

    add templates reviews + clearn code

--- source/reviews/urls.py
+++ source/reviews/urls.py
@@ -1,13 +1,11 @@
-
 from django.urls import path
-from .views import main, index, product_detail, logout_user
+from .views import main, posts, subscriptions, logout_user
 
 
 urlpatterns = [
-    path("", main, name="reviews-main"),
-    path("posts", index, name="reviews-posts"),
-    path("subscriptions", main, name="reviews-subscriptions"),
+    path("flux", main, name="reviews-main"),
+    path("posts", posts, name="reviews-posts"),
+    path("subscriptions", subscriptions, name="reviews-subscriptions"),
     path("logout", logout_user, name="reviews-logout"),
-    path("<str:slug>", product_detail, name="product")
 ] 
 
--- source/reviews/views.py
+++ source/reviews/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from reviews.models import Ticket
+# from django.http import HttpResponse
+# from reviews.models import Ticket
 from django.contrib.auth import logout
 
 def main(request):
-    return render(request, "reviews/index.html")
+    return render(request, "reviews/flux.html")
 
-def index(request):
-    tickets = Ticket.objects.all()
-    return render(request, "reviews/index.html", context = {"tickets": tickets})
+def posts(request):
+    return render(request, "reviews/posts.html")
 
-def product_detail(request, slug):
-    product = get_object_or_404(Ticket, slug=slug)
-    return HttpResponse(product.description)
+def subscriptions(request):
+    return render(request, "reviews/subscriptions.html")
 
 def logout_user(request):
     logout(request)
