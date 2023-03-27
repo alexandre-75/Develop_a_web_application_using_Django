3653b04a3c8efe0c8e31865fc13baadee4c8b4fd
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-27 10:13:16 +0200

    forms.sign_up appli accounts

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from accounts.models import CustomUser
+from django.http import HttpResponse
 
 
 def homepage_welcome(request):
     return render(request, "accounts/homepage.html")
 
 def sign_up(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        if password1 != password2:
+            return render(request, "accounts/sign_up.html", {"error": "mdp no correct"})
+        CustomUser.objects.create_user(username=username, password=password1)
+        return HttpResponse(f"bienvenue {username}")
+
     return render(request, "accounts/sign_up.html")
